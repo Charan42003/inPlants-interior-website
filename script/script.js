6c67dab636bf2a088e28d432e4ce78349b26831e
CHARAN RAJ S <118788971+Charan42003@users.noreply.github.com>
2023-04-21 17:58:05 +0530

    background-script-updated

--- script/script.js
+++ script/script.js
@@ -11,3 +11,11 @@ function menu() {
 function cross() {
     a.style.left = "100vw"
 }
+
+//CHANGING BACKGROUND IMAGES TILL 500PX WIDTH
+if(window.matchMedia("(max-width: 500px)").matches === true) {
+    document.querySelector(".head_bg > img").setAttribute('src', 'images/modern-living-room-right.png');
+}
+else {
+    document.querySelector(".head_bg > img").setAttribute('src', 'images/modern-living-room.png');
+}
